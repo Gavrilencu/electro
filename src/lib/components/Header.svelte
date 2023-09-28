636255f17db4970a256d3d434991134a6ce44b76
Grigore <gavrilencugrigore1998@gmail.com>
2023-09-28 19:41:52 +0300

    v444

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<header class="header">
+  <a href="" class="logo" />
+  <nav class="nav">
+    <a href="" class="list">Home</a>
+    <a href="" class="list">Services</a>
+    <a href="" class="list">Carier</a>
+    <a href="" class="list">Contact</a>
+    <a href="" class="list">Form</a>
+  </nav>
+</header>
+
+<style>
+</style>
